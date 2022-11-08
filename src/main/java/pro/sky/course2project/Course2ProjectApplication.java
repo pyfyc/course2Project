18ca80089af2cebb4deef73fd03bc09d98b4c151
Alex <alexeym75@gmail.com>
2022-11-08 09:49:03 +0700

    Initial commit

---
+++ src/main/java/pro/sky/course2project/Course2ProjectApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.course2project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course2ProjectApplication.class, args);
+    }
+
+}
